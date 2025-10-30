9bf84e2bfbe59a3c5a41d56f1030664e62255d2b
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-30 18:26:16 -0300

    inicio

---
+++ viana-service/src/main/java/com/viana/viana_service/VianaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.viana.viana_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VianaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VianaServiceApplication.class, args);
+	}
+
+}
